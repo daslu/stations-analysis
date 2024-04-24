133128b537262f946bbda849117a79065bb6d283
daslu <daniel.slutsky@gmail.com>
2024-04-24 23:23:51 +0300

    rendered tutorial

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -1,7 +1,7 @@
 (ns index
-  (:require [tablecloth.api :as tc]
-            [aerial.hanami.common :as hc]
-            [aerial.hanami.templates :as ht]))
+  (:require [tablecloth.api :as tc]))
 
 (defonce stations
   (tc/dataset "data/stations20230530.csv.gz"))
+
+(tc/shape stations)
