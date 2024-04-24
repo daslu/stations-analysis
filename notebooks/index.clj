3d6b0197c76c19a50a19b6a1769f9569f9c79e00
daslu <daniel.slutsky@gmail.com>
2024-04-24 23:33:20 +0300

    fixed data reading

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -2,6 +2,12 @@
   (:require [tablecloth.api :as tc]))
 
 (defonce stations
-  (tc/dataset "data/stations20230530.csv.gz"))
+  (tc/dataset "data/stations20230530.csv.gz"
+              {:key-fn keyword
+               :separator "|"}))
 
 (tc/shape stations)
+
+(-> stations
+    (tc/rows :as-maps)
+    first)
