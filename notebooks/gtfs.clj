a6fcbfa96bde6bfaf528c4da0cb1446823778718
daslu <daniel.slutsky@gmail.com>
2024-05-08 01:50:09 +0300

    refactoring

--- notebooks/gtfs.clj
+++ notebooks/gtfs.clj
@@ -116,7 +116,7 @@
       (tc/map-columns :x [:yx] second)
       (tc/map-columns :yishuv [:yx] yx->yishuv)))
 
-(def selected-stops
+(def relevant-stops
   (-> stops-with-location
       (tc/select-rows (fn [{:keys [yishuv]}]
                         (->> yishuv
@@ -131,8 +131,8 @@
                                      7900
                                      9400}))))))
 
-(def selected-stop-ids
-  (-> selected-stops
+(def relevant-stop-ids
+  (-> relevant-stops
       :stop_id
       set))
 
@@ -142,13 +142,20 @@
       (str "stop_times.csv.gz")
       (tc/dataset {:key-fn keyword})))
 
-(def selected-stop-times
+(def relevant-stop-times
   (-> stop-times
       (tc/select-rows
-       #(-> % :stop_id selected-stop-ids))))
+       #(-> % :stop_id relevant-stop-ids))))
+
+
+
+
+
+
+
 
 (def edges
-  (-> selected-stop-times
+  (-> relevant-stop-times
       (tc/group-by [:trip_id] {:result-type :as-seq})
       (->> (mapcat (fn [{:keys [stop_id]}]
                      (map (comp sort vector)
@@ -158,7 +165,7 @@
       distinct))
 
 (def vertices
-  (->> selected-stops
+  (->> relevant-stops
        :stop_id
        distinct
        sort))
@@ -167,14 +174,14 @@
 (count vertices)
 
 
-(def selected-stops-lat-lon
-  (-> selected-stops
+(def relevant-stops-lat-lon
+  (-> relevant-stops
       (tc/select-columns [:stop_id :stop_lat :stop_lon])
       (tc/set-dataset-name nil)))
 
 (def selectesd-stop-times-lat-lon
-  (-> selected-stop-times
-      (tc/left-join selected-stops-lat-lon [:stop_id])))
+  (-> relevant-stop-times
+      (tc/left-join relevant-stops-lat-lon [:stop_id])))
 
 (def edges-lat-lon
   (-> edges
@@ -182,8 +189,8 @@
                   {:v0 v0
                    :v1 v1})))
       tc/dataset
-      (tc/left-join selected-stops-lat-lon {:left :v0 :right :stop_id})
-      (tc/left-join selected-stops-lat-lon {:left :v1 :right :stop_id})
+      (tc/left-join relevant-stops-lat-lon {:left :v0 :right :stop_id})
+      (tc/left-join relevant-stops-lat-lon {:left :v1 :right :stop_id})
       (tc/select-columns [:stop_lat :stop_lon
                           :.stop_lat :.stop_lon])
       (tc/rename-columns {:stop_lat :stop_lat0
@@ -210,7 +217,7 @@
 
 
 (def scored-stops
-  (-> selected-stops
+  (-> relevant-stops
       (tc/map-columns :betweeness [:stop_id] betweeness)
       (tc/log :log-betweeness :betweeness)))
 
@@ -234,14 +241,15 @@
       (assoc :projection {:type :mercator})))
 
 
-(-> scored-stops
-    (tc/map-columns :central [:betweeness] #(> % 0.001))
-    (hanami/plot ht/point-chart
-                 {:X "stop_lat"
-                  :Y "stop_lon"
-                  :COLOR "central"
-                  :OPACITY 0.1})
-    as-geo)
+(delay
+  (-> scored-stops
+      (tc/map-columns :central [:betweeness] #(> % 0.002))
+      (hanami/plot ht/point-chart
+                   {:X "stop_lat"
+                    :Y "stop_lon"
+                    :COLOR "central"
+                    :OPACITY 0.1})
+      as-geo))
 
 (defn leaflet-map [geojson]
   (kind/reagent
@@ -287,30 +295,31 @@
 (delay
   (leaflet-map
    {:type :FeatureCollection
-    :features (vec
-               (concat (-> edges-lat-lon
-                           (tc/rows :as-maps)
-                           (->> (mapv (fn [{:keys [stop_lat0 stop_lon0 stop_lat1 stop_lon1]}]
-                                        {:type :Feature
-                                         :geometry {:type :LineString
-                                                    :coordinates [[stop_lon0 stop_lat0]
-                                                                  [stop_lon1 stop_lat1]]}}))))
-                       (-> scored-stops
-                           (tc/rows :as-maps)
-                           (->> (mapv (fn [{:keys [stop_name stop_lat stop_lon betweeness]}]
-                                        (let [radius (if (> betweeness 0.002)
-                                                       50
-                                                       20)
-                                              color (if (> betweeness 0.002)
-                                                      "yellow"
-                                                      "cyan")]
-                                          {:type :Feature
-                                           :geometry {:type :Point
-                                                      :coordinates [stop_lon stop_lat]}
-                                           :properties {:style {:radius radius
-                                                                :fillColor color
-                                                                :color color
-                                                                :weight 1
-                                                                :opacity 1
-                                                                :fillOpacity 0.5}
-                                                        :tooltip (str stop_name " " (format "%.02f%%" (* 100 betweeness)))}})))))))}))
+    :features
+    (vec
+     (concat (-> edges-lat-lon
+                 (tc/rows :as-maps)
+                 (->> (mapv (fn [{:keys [stop_lat0 stop_lon0 stop_lat1 stop_lon1]}]
+                              {:type :Feature
+                               :geometry {:type :LineString
+                                          :coordinates [[stop_lon0 stop_lat0]
+                                                        [stop_lon1 stop_lat1]]}}))))
+             (-> scored-stops
+                 (tc/rows :as-maps)
+                 (->> (mapv (fn [{:keys [stop_name stop_lat stop_lon betweeness]}]
+                              (let [radius (if (> betweeness 0.002)
+                                             50
+                                             20)
+                                    color (if (> betweeness 0.002)
+                                            "yellow"
+                                            "cyan")]
+                                {:type :Feature
+                                 :geometry {:type :Point
+                                            :coordinates [stop_lon stop_lat]}
+                                 :properties {:style {:radius radius
+                                                      :fillColor color
+                                                      :color color
+                                                      :weight 1
+                                                      :opacity 1
+                                                      :fillOpacity 0.5}
+                                              :tooltip (str stop_name " " (format "%.02f%%" (* 100 betweeness)))}})))))))}))
